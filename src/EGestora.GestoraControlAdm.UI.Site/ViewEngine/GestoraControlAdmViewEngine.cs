043bfd9def61406a144223d9fb642e7d016ef32b
jbrambilla <joao.brambilla@egestora.com.br>
2016-02-24 16:53:26 -0300

    refatoração de views de clientes

--- src/EGestora.GestoraControlAdm.UI.Site/ViewEngine/GestoraControlAdmViewEngine.cs
+++ src/EGestora.GestoraControlAdm.UI.Site/ViewEngine/GestoraControlAdmViewEngine.cs
@@ -8,6 +8,8 @@ namespace EGestora.GestoraControlAdm.UI.Site.ViewEngine
     {
         private static string[] NewPartialViewFormats = new[] { 
             "~/Views/{1}/Partials/{0}.cshtml",
+            "~/Views/Clientes/Cnaes/{0}.cshtml",
+            "~/Views/Clientes/Servicos/{0}.cshtml",
             "~/Views/Shared/Pessoa/{0}.cshtml",
             "~/Views/Shared/Pessoa/Endereco/{0}.cshtml"
         };

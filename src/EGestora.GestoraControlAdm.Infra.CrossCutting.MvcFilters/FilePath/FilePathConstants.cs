e8ac743dc6a1b8b6560dd19b10eafa4298224cf1
jbrambilla <joao.brambilla@egestora.com.br>
2016-03-03 15:16:34 -0300

    filepath

---
+++ src/EGestora.GestoraControlAdm.Infra.CrossCutting.MvcFilters/FilePath/FilePathConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace EGestora.GestoraControlAdm.Infra.CrossCutting.MvcFilters.FilePath
+{
+    public class FilePathConstants
+    {
+        public const string CLIENTES_IMAGE_PATH = @"C:\EGestora\GestoraControlAdm\src\contents\clientes\";
+        public const string FORNECEDORES_IMAGE_PATH = @"C:\EGestora\GestoraControlAdm\src\contents\fornecedores\";
+        public const string REVENDAS_IMAGE_PATH = @"C:\EGestora\GestoraControlAdm\src\contents\revendas\";
+        public const string EMPRESAS_IMAGE_PATH = @"C:\EGestora\GestoraControlAdm\src\contents\empresas\";
+        public const string FUNCIONARIOS_IMAGE_PATH = @"C:\EGestora\GestoraControlAdm\src\contents\funcionarios\";
+    }
+}

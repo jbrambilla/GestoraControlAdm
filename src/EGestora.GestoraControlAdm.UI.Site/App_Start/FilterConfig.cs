0e2dc4ce6ac38474c488600918149c2e39e27e70
jbrambilla <joao.brambilla@egestora.com.br>
2016-04-14 15:11:16 -0300

    Autenticação de usuario colocada em funcionamento; - Migrations do Identity ativada;

--- src/EGestora.GestoraControlAdm.UI.Site/App_Start/FilterConfig.cs
+++ src/EGestora.GestoraControlAdm.UI.Site/App_Start/FilterConfig.cs
@@ -8,6 +8,8 @@ namespace EGestora.GestoraControlAdm.UI.Site
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
--- src/EGestora.GestoraControlAdm.UI.Site/App_Start/SimpleInjectorInitializer.cs
+++ src/EGestora.GestoraControlAdm.UI.Site/App_Start/SimpleInjectorInitializer.cs
@@ -4,12 +4,13 @@ namespace EGestora.GestoraControlAdm.UI.Site.App_Start
 {
     using System.Reflection;
     using System.Web.Mvc;
-
     using SimpleInjector;
     using SimpleInjector.Extensions;
     using SimpleInjector.Integration.Web;
     using SimpleInjector.Integration.Web.Mvc;
     using EGestora.GestoraControlAdm.Infra.CrossCutting.IoC;
+    using System.Web;
+    using Microsoft.Owin;
     
     public static class SimpleInjectorInitializer
     {
@@ -20,6 +21,16 @@ namespace EGestora.GestoraControlAdm.UI.Site.App_Start
             container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
             
             InitializeContainer(container);
+
+            container.RegisterPerWebRequest(() =>
+            {
+                if (HttpContext.Current != null && HttpContext.Current.Items["owin.Environment"] == null && container.IsVerifying)
+                {
+                    return new OwinContext().Authentication;
+                }
+                return HttpContext.Current.GetOwinContext().Authentication;
+
+            });
 
             container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
             

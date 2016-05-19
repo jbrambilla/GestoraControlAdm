83528ec537478881f155df64cf0bf75c3ceb480b
jbrambilla <joao.brambilla@egestora.com.br>
2016-05-19 09:22:55 -0300

    Blank page e pagina de login

---
+++ src/EGestora.GestoraControlAdm.UI.SmartSite/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EGestora.GestoraControlAdm.UI.SmartSite.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
